dd223ed68c77d408fcbd284bf280d8596e02144a
Boris Jonica <boris.jonica@utexas.edu>
2015-04-18 16:04:39 -0500

    add eventEmitter, rename to use twit client. Should work the same?

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 "use strict";
 var thenify = require('thenify');
+var EventEmitter = require('events').EventEmitter;
 
-module.exports = function(client) {
-    var wrap = {};
+module.exports = function (client) {
+  var wrap = {};
 
-    Object.keys(client).forEach(function (key) {
-        wrap[key] = client[key];
-    });
+  Object.keys(client).forEach(function (key) {
+    wrap[key] = client[key];
+  });
 
-    Object.keys(Object.getPrototypeOf(client)).forEach(function (key) {
-        wrap[key] = thenify(client[key].bind(client));
-    });
+  Object.keys(EventEmitter.prototype).forEach(function (key) {
+    if (typeof client[key] == 'function') {
+      wrap[key] = client[key].bind(client);
+    }
+  });
 
-    return wrap;
+  Object.keys(Object.getPrototypeOf(client)).forEach(function (key) {
+    wrap[key] = thenify(client[key].bind(client));
+  });
+
+  return wrap;
 };
